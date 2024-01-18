c7839a965371cf04c2711a747200cecea8849f39
Luca Spinazza <66678266+lspnzz@users.noreply.github.com>
2024-01-18 16:43:41 +0100

    Update home-animations.js

--- home-animations.js
+++ home-animations.js
@@ -1,43 +1,85 @@
 window.addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(ScrollTrigger);
-  const imageToBeClipped = document.getElementById("clipped-image");
+  gsap.registerPlugin(Flip);
 
-  gsap.fromTo(
-    imageToBeClipped,
-    {
-      clipPath: "polygon(0% 0%, 0% 0%, 100% 0%, 100% 100%, 100% 100%, 0% 100%)",
-    },
-    {
-      scrollTrigger: {
-        trigger: imageToBeClipped,
-        start: "top 80%",
-        end: "top 20%",
-        once: true,
-      },
-
-      clipPath: "polygon(0% 30%, 50% 0%, 100% 0%, 100% 70%, 50% 100%, 0% 100%)",
-      duration: 2,
-      ease: "power4.out",
-    }
+  const videoWrapperAtf = document.querySelector(".atf-video_wrapper");
+  const trLogoAtf = document.querySelector("#tr-logo-atf");
+  const navbarLinks = document.querySelectorAll(".navbar_link");
+  const navbarLineHorizontal = document.querySelector(
+    "#section-line-horizontal-navbar"
   );
-});
+  const navbarHighlight = document.querySelector(".navbar_highlight");
 
-window.addEventListener("DOMContentLoaded", () => {
-  gsap.registerPlugin(ScrollTrigger);
-
-  // Select all elements with the class '.section_image'
-  document.querySelectorAll(".section_image").forEach((image) => {
-    gsap.to(image, {
-      scrollTrigger: {
-        trigger: image,
-        start: "top 80%",
-        end: "top center",
-        scrub: true,
-        once: true,
-      },
-      padding: "0rem",
-      duration: 2,
-      ease: "linear",
-    });
+  const openingTl = gsap.timeline({
+    defaults: {
+      duration: 1.75,
+      ease: "circ.inOut",
+    },
   });
+
+  // (LS): Video wrapper animation.
+  openingTl
+    .from(
+      videoWrapperAtf,
+      {
+        width: "140%",
+        x: "-15%",
+        y: "-20%",
+      },
+      "<"
+    )
+    .fromTo(
+      videoWrapperAtf,
+      {
+        clipPath:
+          "polygon(0% 0%, 0% 0%, 100% 0%, 100% 100%, 100% 100%, 0% 100%)",
+      },
+      {
+        clipPath:
+          "polygon(0% 30%, 50% 0%, 100% 0%, 100% 70%, 50% 100%, 0% 100%)",
+      },
+      "<"
+    );
+
+  // (LS): Navbar animation.
+  openingTl
+    .from(
+      navbarLineHorizontal,
+      {
+        right: "auto",
+      },
+      "0"
+    )
+    .from(
+      navbarLinks,
+      {
+        opacity: 0,
+        y: "1rem",
+        stagger: 0.1,
+      },
+      "<"
+    )
+    .from(navbarHighlight, { scaleX: 0, opacity: 0 }, "<+0.4");
+
+  openingTl.from(trLogoAtf, { y: "3rem" }, "0");
 });
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   gsap.registerPlugin(ScrollTrigger);
+
+//   // Select all elements with the class '.section_image'
+//   document.querySelectorAll(".section_image").forEach((image) => {
+//     gsap.to(image, {
+//       scrollTrigger: {
+//         trigger: image,
+//         start: "top 80%",
+//         end: "top center",
+//         scrub: true,
+//         once: true,
+//       },
+//       padding: "0rem",
+//       duration: 2,
+//       ease: "linear",
+//     });
+//   });
+// });
