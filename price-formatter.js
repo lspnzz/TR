46cd7dc15fc7aebcaa31a5cb61cff4f383589cf1
Luca Spinazza <66678266+lspnzz@users.noreply.github.com>
2024-01-19 14:39:31 +0100

    Update price-formatter.js

--- price-formatter.js
+++ price-formatter.js
@@ -1,20 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   const priceTags = document.querySelectorAll("[price-tag]");
 
-  function formatPrice(price) {
+  function formatPrice(price, decimalPlaces) {
     const formatter = new Intl.NumberFormat("en-IE", {
       style: "currency",
       currency: "EUR",
-      minimumFractionDigits: 2,
+      minimumFractionDigits: decimalPlaces,
+      maximumFractionDigits: decimalPlaces,
     });
 
     return formatter.format(price);
   }
 
   if (priceTags.length > 0) {
-    priceTags.forEach((priceTag) => {
+    priceTags.forEach(function (priceTag) {
       const rawPrice = parseFloat(priceTag.textContent);
-      const formattedPrice = formatPrice(rawPrice);
+      const decimalPlaces = priceTag.hasAttribute("round-to-decimals")
+        ? parseInt(priceTag.getAttribute("round-to-decimals"), 10)
+        : 2;
+      const formattedPrice = formatPrice(rawPrice, decimalPlaces);
       priceTag.textContent = formattedPrice;
     });
   }
