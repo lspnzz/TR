12c1c23e476210ece4322ddf7afd7d18e0881eb5
Luca Spinazza <66678266+lspnzz@users.noreply.github.com>
2024-01-19 14:12:24 +0100

    Create price-formatter.js

---
+++ price-formatter.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const priceTags = document.querySelectorAll("[price-tag]");
+
+  function formatPrice(price) {
+    const formatter = new Intl.NumberFormat("en-IE", {
+      style: "currency",
+      currency: "EUR",
+      minimumFractionDigits: 2,
+    });
+
+    return formatter.format(price);
+  }
+
+  if (priceTags.length > 0) {
+    priceTags.array.forEach((priceTag) => {
+      const rawPrice = parseFloat(priceTag.textContent);
+      const formattedPrice = formatPrice(rawPrice);
+      priceTag.textContent = formattedPrice;
+    });
+  }
+});
